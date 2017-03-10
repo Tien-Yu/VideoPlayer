5e8b525c832f1553e64bf21caf50fe7fc95131ee
TienYu <skyand55@gmail.com>
2017-03-11 02:34:18 +0800

    half of shuffle complete

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,23 +1,29 @@
 
 var currVideo = document.getElementById("mainPlayer");
+var trackNames = document.getElementById("playList");
+var lastTrack = 3;
+var currentTrack = 3;
 
-function changeVideo(vid){
-	var prepareVideo = document.getElementById(vid)
+$(".prepareList").click(function(){
+	var prepareVideo = document.getElementById(this.id);
+	document.getElementById(this.id+"o").selected=true;
 	currVideo.src = prepareVideo.src;
+	
 	// reset the speed
 	$("#speedRange").val(1);
 	$("#speedShow").val(1);
 	currVideo.playbackRate = 1;
 	
 	currVideo.play();
-}
+});
 
+// Speed bar
 $("#speedRange").change(function(){
 	$("#speedShow").val($(this).val());
 	currVideo.playbackRate = $(this).val();
-})
+});
 
-
+// Prepare list
 $(".prepareList").on({
 	mouseenter: function(){
 		$(this).css("cursor", "pointer");
@@ -25,8 +31,8 @@ $(".prepareList").on({
 	mouseleave: function(){
 	$(this).css("cursor", "auto");	
 	}
-})
-
+});
+// Player
 $("#mainPlayer").on({
 	mouseenter: function(){ //mouseenter, mouseleave一起用代表hover
 		$(this).css("cursor", "pointer");
@@ -42,8 +48,41 @@ $("#mainPlayer").on({
 			this.play();
 		}	
 	}
-	
 });
 
+// Shuffle play
+$("#shuffle").on({
+	click: function(){
+		$(this).attr("src", "pics/shuffle_c.png");
+		lastTrack = currentTrack;
+		currVideo.addEventListener('ended', function() { //Callback
+			var timee = new Date(); //用來亂數
+			trackNames.selectedIndex = timee.getTime() % trackNames.length;
+			currentTrack = trackNames.selectedIndex;
+			
+			// not allow same track
+			while(lastTrack==currentTrack){
+				timee = new Date();
+				trackNames.selectedIndex = timee.getTime() % trackNames.length;
+				currentTrack = trackNames.selectedIndex;
+			}
+			// 修改路徑
+			this.src = "./videos/" + trackNames[currentTrack].value;
+			this.play();
+			// reset the speed
+			$("#speedRange").val(1);
+			$("#speedShow").val(1);
+			this.playbackRate = 1;
+			//update lastTrack
+			lastTrack = currentTrack;
+		})
+	},
+	mouseenter: function(){ //mouseenter, mouseleave一起用代表hover
+		$(this).css("cursor", "pointer");
+	},
+	mouseleave: function(){
+		$(this).css("cursor", "auto");
+	}
+});
 
 
